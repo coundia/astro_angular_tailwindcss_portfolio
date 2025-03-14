fef32cac3583f256b6ac8f00e52037b27031943f
pcoundia <papacoundia@gmail.com>
2025-03-14 18:38:23 +0000

    feat: translate navbar

--- src/data/navbar.ts
+++ src/data/navbar.ts
@@ -1,3 +1,19 @@
-export const navbar = {
 
-};
+export  const titles = {
+	en: {
+		home: "Home",
+		projects: "Projects",
+		skills: "Skills",
+		about: "About",
+		contact: "Contact",
+		blog: "Blog"
+	},
+	fr: {
+		home: "Accueil",
+		projects: "Projets",
+		skills: "Compétences",
+		about: "À propos",
+		contact: "Contact",
+		blog: "Blog"
+	}
+}

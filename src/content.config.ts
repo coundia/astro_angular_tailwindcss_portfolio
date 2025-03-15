46eb5bdbc4dd2030a80f7c471f8b12ff277c3340
pcoundia <papacoundia@gmail.com>
2025-03-15 10:41:53 +0000

    bugfix: fix blog slug

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,21 +1,18 @@
-// 1. Import utilities from `astro:content`
-import { defineCollection, z } from "astro:content";
+import { glob } from 'astro/loaders';
+import { defineCollection, z } from 'astro:content';
 
-// 2. Import loader(s)
-import { glob } from "astro/loaders";
-
-// 3. Define your collection(s)
 const blog = defineCollection({
-    loader: glob({
-        pattern: "**/*.md",
-        base: "./src/content/blog",
-    }),
-    schema: z.object({
-        title: z.string(),
-        date: z.string(),
-        excerpt: z.string(),
-        tags: z.array(z.string()).optional(),
-    }),
+	// Load Markdown and MDX files in the `src/content/blog/` directory.
+	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+	}),
 });
-// 4. Export a single `collections` object to register your collection(s)
+
 export const collections = { blog };
--- src/content/blog/post2.md
+++
@@ -1,7 +0,0 @@
----
-title: "Premier article"
-date: "2025-03-15"
-excerpt: "Un aperçu de mon premier article."
-tags: ["Astro", "Angular"]
----
-Contenu de l'article...

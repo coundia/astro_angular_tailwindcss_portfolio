5b581a471480036b9825b1464b262b076add6959
pcoundia <papacoundia@gmail.com>
2025-04-16 08:50:34 +0000

    feat: add projects

--- src/data/Projects.ts
+++ src/data/Projects.ts
@@ -1,10 +1,11 @@
 export const Projects = [
+
 	{
-		title: "DDD Maker Bundle",
-		logo: "ddd-maker-bundle.png",
-		description: "Un bundle Symfony open-source pour générer du code DDD (Domain-Driven Design) avec le modèle CQRS.",
-		resume: "Automatisation de la génération de code DDD en Symfony.",
-		link: "https://packagist.org/packages/cnd/ddd-maker-bundle",
+		title: "Co-funder Megastore SN",
+		logo: "megastore.png",
+		description: "Plateforme e-commerce proposant une large gamme de produits avec une boutique physique au Sénégal.",
+		resume: "E-commerce et magasin physique au Sénégal.",
+		link: "https://www.megastore.sn",
 	},
 	{
 		title: "Groupe 2CS",
@@ -14,11 +15,35 @@ export const Projects = [
 		link: "https://www.groupe2cs.com",
 	},
 	{
-		title: "Co-funder Megastore SN",
-		logo: "megastore.png",
-		description: "Plateforme e-commerce proposant une large gamme de produits avec une boutique physique au Sénégal.",
-		resume: "E-commerce et magasin physique au Sénégal.",
-		link: "https://www.megastore.sn",
+		title: "Générateur code DDD avec Spring Boot",
+		logo: "ddd-maker-bundle.png",
+		description: "Axon Web Code Generator automatise la génération de projets backend en CQRS + DDD avec Spring Boot, " +
+			"Axon et RabbitMQ. Un outil puissant pour industrialiser le développement orienté domaine." +
+			"Ce projet est en constante évolution.\n" +
+			"\n" +
+			"Pour plus de détails voir l'article: https://www.pcoundia.com/blog/jpa-axon-web-cqrs-generator" +
+			"",
+		resume: "Automatisation de la génération de code DDD en Spring.",
+		link: "https://github.com/coundia/axon-web-cqrs-generator",
 	},
+	{
+		title: "Générateur code DDD avec Spring Boot Webflux, 100% réactif",
+		logo: "ddd-maker-bundle.png",
+		description: "Axon WebFlux Code Generator automatise la génération de projets backend en CQRS + DDD avec Spring Boot, " +
+			"Axon et RabbitMQ 100% réactifs. Un outil puissant pour industrialiser le développement orienté domaine." +
+			"Ce projet est en constante évolution.\n" +
+			"\n" +
+			"",
+		resume: "Automatisation de la génération de code DDD en Spring.",
+		link: "https://github.com/coundia/axon-webflux-cqrs-generator",
+	},
+	{
+		title: "DDD Maker Bundle",
+		logo: "ddd-maker-bundle.png",
+		description: "Un bundle Symfony open-source pour générer du code DDD (Domain-Driven Design) avec le modèle CQRS.",
+		resume: "Automatisation de la génération de code DDD en Symfony.",
+		link: "https://packagist.org/packages/cnd/ddd-maker-bundle",
+	},
+
 
 ];
